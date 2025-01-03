763570488ec2b7e59c8c9a7f07364615e6da8914
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-01-03 12:06:17 +0530

    first commit

---
+++ src/main/java/com/example/demo/PosSystemServiceMenuwarageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemServiceMenuwarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSystemServiceMenuwarageApplication.class, args);
+	}
+
+}
